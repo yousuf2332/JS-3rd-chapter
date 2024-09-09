14898a2808590f3993c648d3660ee5d8c6374eac
yousuf2332 <yousuf23325268@gmail.com>
2024-09-09 11:04:04 -0700

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+var age = 17;
+alert("I am " + age + "  years old");
+
+var visit = 16;
+alert("You have visited this site " + visit + " times.");
+
+var birthyear = 2006;
+document.getElementById("demo").innerHTML = "My birth year is " + birthyear + "\n Data type of my declared variable is number";
+
+var visitorName = "yousuf";
+var productTitle = "MS";
+var quantity = 40;
+
+console.log(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ FOOD Brand.");
